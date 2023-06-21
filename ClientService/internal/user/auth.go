c7f1dd10d49d1588a189ce0cad20fb4e7d498cd0
SacredStar <shmelevka93@yandex.ru>
2023-06-21 12:14:28 +0300

    Name refactoring

--- ClientService/internal/user/auth.go
+++ ClientService/internal/user/auth.go
@@ -8,8 +8,6 @@ import (
 
 type Auth struct {
 	token      string
-	AlgID      string
-	Provider   string
 	ServerHost string
 	ServerPort string
 }
@@ -17,11 +15,16 @@ type Auth struct {
 func (a *Auth) Authentificate(login, password string) {
 	//TODO: Send login password to auth server, return token
 	//TODO: set info from config?
-	conn, _ := net.Dial("tcp", "0.0.0.0:2000") // открываем TCP-соединение к серверу
+	conn, err := net.Dial("tcp", "0.0.0.0:2000") // открываем TCP-соединение к серверу
+	if err != nil {
+		fmt.Println("error connecting to Auth server")
+		return
+	}
 	for {
 		data := []byte(login + " " + password + "\n")
 		if _, err := conn.Write(data); err != nil {
 			fmt.Println("Error sending login/password to auth server")
+			return
 		}
 		token, _ := bufio.NewReader(conn).ReadBytes('.')
 		a.token = string(token)
